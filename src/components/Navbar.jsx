81854612453f3365079287f9d597bc57f0e9ea31
mhdalihoria <alihoria123456@gmail.com>
2025-04-06 20:09:56 +0300

    More Addition to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,84 @@
-    import React, { useState, useEffect } from "react";
-    import { RxHamburgerMenu } from "react-icons/rx";
-    import "./navbar.css";
-    import { FaArrowRightLong } from "react-icons/fa6";
+import React, { useState, useEffect } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+import "./navbar.css";
+import { FaArrowRightLong } from "react-icons/fa6";
+import { Offcanvas } from "react-bootstrap";
+import { IoMdClose } from "react-icons/io";
 
-    export default function Navbar() {
-    const [show, setShow] = useState(true);
-    const [lastScrollY, setLastScrollY] = useState(0);
+export default function Navbar() {
+  const [show, setShow] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [showOffcanvas, setShowOffcanvas] = useState(false);
 
-    useEffect(() => {
-        const handleScroll = () => {
-        const currentScrollY = window.scrollY;
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
 
-        if (currentScrollY > lastScrollY && currentScrollY > 50) {
-            setShow(false); // scrolling down
-        } else {
-            setShow(true); // scrolling up
-        }
+      if (currentScrollY > lastScrollY && currentScrollY > 50) {
+        setShow(false); // scrolling down
+      } else {
+        setShow(true); // scrolling up
+      }
 
-        setLastScrollY(currentScrollY);
-        };
+      setLastScrollY(currentScrollY);
+    };
 
-        window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, [lastScrollY]);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [lastScrollY]);
 
-    return (
-        <nav className={`navbar ${show ? "visible" : "hidden"}`}>
-        <div className="nav-hamburger">
-            <RxHamburgerMenu />
+  const navItems = [
+    {
+      label: "Rooms",
+      link: "/",
+    },
+    {
+      label: "Directions",
+      link: "/",
+    },
+    {
+      label: "Contact Us",
+      link: "/",
+    },
+  ];
+
+  return (
+    <>
+      <nav className={`navbar ${show ? "visible" : "hidden"}`}>
+        <div className="nav-hamburger" onClick={() => setShowOffcanvas(true)}>
+          <RxHamburgerMenu />
         </div>
 
         {/* <div>Logo</div> */}
         <button className="nav-cta">
-            Book a Room
-            {/* <FaArrowRightLong style={{ marginLeft: "10px", fill: "#ffc814" }} /> */}
+          Book a Room
+          {/* <FaArrowRightLong style={{ marginLeft: "10px", fill: "#ffc814" }} /> */}
         </button>
-        </nav>
-    );
-    }
+      </nav>
+
+      <Offcanvas
+        show={showOffcanvas}
+        onHide={() => setShowOffcanvas(false)}
+        placement="top"
+      >
+        <Offcanvas.Header>
+          <button
+            onClick={() => setShowOffcanvas(false)}
+            className="custom-close-btn"
+            aria-label="Close"
+          >
+            <IoMdClose />
+          </button>{" "}
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <div className="nav-items">
+            {navItems.map((item) => (
+              <span>{item.label}</span>
+            ))}
+          </div>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
+  );
+}
