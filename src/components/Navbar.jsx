69798cb833409cdba7e078782465ed28ad91ad4f
mhdalihoria <alihoria123456@gmail.com>
2025-04-06 20:50:57 +0300

    Adding Carousel to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,42 @@
 import React, { useState, useEffect } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
-import "./navbar.css";
 import { FaArrowRightLong } from "react-icons/fa6";
 import { Offcanvas } from "react-bootstrap";
 import { IoMdClose } from "react-icons/io";
+import SingleRoom from "../assets/single-room.jpg";
+
+import "./navbar.css";
+import Slider from "react-slick";
 
 export default function Navbar() {
+  const settings = {
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    infinite: true,
+    slidesToShow: 2, // We'll adjust number with CSS if needed
+    slidesToScroll: 1,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          variableWidth: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          variableWidth: true,
+        },
+      },
+    ],
+  };
+  // --------------------------------------
   const [show, setShow] = useState(true);
   const [lastScrollY, setLastScrollY] = useState(0);
   const [showOffcanvas, setShowOffcanvas] = useState(false);
@@ -41,6 +72,25 @@ export default function Navbar() {
       label: "Contact Us",
       link: "/",
     },
+    {
+      label: "Rooms",
+      link: "/",
+    },
+    {
+      label: "Directions",
+      link: "/",
+    },
+    {
+      label: "Contact Us",
+      link: "/",
+    },
+  ];
+
+  const roomTypes = [
+    { img: SingleRoom, name: "Single Room" },
+    { img: SingleRoom, name: "Double Room" },
+    { img: SingleRoom, name: "Twin Room" },
+    { img: SingleRoom, name: "Meeting Room" },
   ];
 
   return (
@@ -76,6 +126,21 @@ export default function Navbar() {
             {navItems.map((item) => (
               <span>{item.label}</span>
             ))}
+          </div>
+          <div className="section-content">
+            <div className="section-title">
+              <span className="title">
+                Rooms <span className="and">&</span> Halls{" "}
+              </span>
+            </div>
+            <Slider {...settings} className="slider-container">
+              {roomTypes.map((room) => (
+                <div key={room.name} className="room-container">
+                  <img src={room.img} alt={room.name} className="room-img" />
+                  <p className="room-title">{room.name}</p>
+                </div>
+              ))}
+            </Slider>
           </div>
         </Offcanvas.Body>
       </Offcanvas>
