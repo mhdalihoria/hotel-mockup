931e0a91bacebce51d8cde0329bd8047a2fefe98
mhdalihoria <alihoria123456@gmail.com>
2025-04-06 14:31:55 +0300

    Adding Layout (for navbar and footer)

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import "./index.css";
 import App from "./App.jsx";
 import RoomPageLayout from "./layouts/RoomPageLayout.jsx";
 import SingleRoom from "./SingleRoom.jsx";
+import MainLayout from "./layouts/MainLayout.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/single-room" element={<SingleRoom />} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<App />} />
+          <Route path="/single-room" element={<SingleRoom />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
