f266b1afe8adbfae414d2f7c564f48e0811bb582
mhdalihoria <alihoria123456@gmail.com>
2025-04-08 13:59:11 +0300

    implemented animation for the sight seeing

--- src/components/homepage/Sightseeing.jsx
+++ src/components/homepage/Sightseeing.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Place1 from "../../assets/place1.jpg";
 import "./homepage.css";
 
-
 export default function Sightseeing() {
   const [openIndex, setOpenIndex] = useState(0);
   const cardsArr = [
@@ -26,18 +25,20 @@ export default function Sightseeing() {
             onClick={() => handleClick(idx)}
           >
             <img src={card.img} className="card-img" />
-            {openIndex === idx && (
-              <div className="card-content-container">
-                <div className="card-content-content">
-                  <h1 className="card-header">Lorem Ipsum</h1>
-                  <p className="card-paragraph">
-                    Nujuma, a Ritz-Carlton Reserve is rooted in rich cultural
-                    traditions, pristine natural beauty and flavors as vivid as
-                    the sea itself.
-                  </p>
-                </div>
+            <div
+              className={`card-content-container ${
+                openIndex === idx ? "visible" : ""
+              }`}
+            >
+              <div className="card-content-content">
+                <h1 className="card-header">Lorem Ipsum</h1>
+                <p className="card-paragraph">
+                  Nujuma, a Ritz-Carlton Reserve is rooted in rich cultural
+                  traditions, pristine natural beauty and flavors as vivid as
+                  the sea itself.
+                </p>
               </div>
-            )}
+            </div>
           </div>
         ))}
       </div>
